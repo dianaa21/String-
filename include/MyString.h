bc7efad82af4d53ad13e364731f7756ccf21cd26
dianaa21 <aminova.diana@live.ru>
2016-04-21 18:29:09 +0300

    21.04 18.29

--- include/MyString.h
+++ include/MyString.h
@@ -1,53 +1,194 @@
 #ifndef CPP_STRING_H
 #define CPP_STRING_H
-//one definition rule
+#include <stddef.h>
+#include <stdexcept>
+
 
 class String {
 private:
-    size_t length; //length of actual string, w/o EOL symvol
+    int l;
     char * string;
 public:
     static char position;
-    String();
-    String(const char *str);
-    String(const char *str, unsigned count);
-    String(char ch, unsigned count);
-    String(const String &other);
-    String(String &&other);
-    ~String();
-    String & operator=(const String &other);
-    String & operator=(String &&other);
-
-    String & operator+=(const String &suffix);
-    String & operator+=(const char *suffix);
-    String & operator+=(char suffix);
-    void swap(String &other);
-    char & operator[](size_t pos);
-    const char operator[](size_t pos) const;
-/**
-throws an exception if pos >= size()
-*/
-    char & at(size_t pos);
-
-/**
-throws an exception if pos >= size()
-*/
-    const char at(size_t pos) const;
-/**
-/return pointer to '\0' terminated C-style string
-*/
-    const char * data() const;
-    size_t size() const;
-    friend bool operator==(const String &lhs, const String &rhs);
-    friend bool operator<(const String &lhs, const String &rhs);
+    String(){
+		l=0;
+		string=new char[1];
+		string[0]='\0';
+	};
+    String(const char *s){
+	   l=0;
+		const char *t = str;
+		while (*(t++) != '\0')
+			l++;
+		   char *str = new char[l + 1];
+		for (int i = 0; i < l; i++)
+			str[i] = s[i];
+    string = str
+    string[l] = '\0';
+	};
+    String(const char *str, unsigned count){
+	 string = new char[count + 1];
+    l = count;
+    for (int i = 0; i <= l; i++)
+        string[i] = str[i];
+    };
+    String(char ch, unsigned count){
+		l = count;
+		string = new char[l + 1];
+		char *t = string;
+      for (int i = 0; i <= l; i++)
+        t[i] = ch;
+		t[l] = '\0';
+	};
+    String(const String &other){
+		string = new char[other.l + 1];
+		l = other.l;
+		for (int i = 0; i <= l; i++)
+			string[i] = other.string[i];
+	};
+    String(String &&other)
+	 {
+	 string = other.string;
+    l = other.l;
+    other.string = '\0';
+    other.l = 0;
+	 };
+    ~String(){
+        delete[] string;
+    };
+    String & operator=(const String &other){
+	    delete[] string;
+		 string=new char[other.l + 1];
+		 l=other.l;
+		for (int i = 0; i < l; i++)
+        string[i]=other.string[i];
+		string[l] = '\0';
+    return *this;
+	 };
+    String & operator=(String &&other){
+	     delete[] string;
+		string = other.string;
+		l = other.l;
+		other.string = '\0';
+		other.l = 0;
+		return *this;
+	 };
+    String & operator+=(const String &suffix){  
+	 char *concat = new char[l + suffix.l + 1];
+    for (int i = 0; i < l; ++i)
+        concat[i] = string[i];
+    for (int j = l; j < l + suffix.l; ++j)
+        concat[j] = suffix.string[j - l];
+    concat[l + suffix.l] = '\0';
+    delete[] string;
+    string = concat;
+    l += suffix.l;
+    return *this;
+	 };
+    String & operator+=(const char *suffix){
+	  int suffixl = 0;
+    const char *temp = suffix;
+    while (*(temp++) != '\0') {
+        suffixl++;
+		  char *concat = new char[l + suffixl + 1];
+    for (int i = 0; i < l; ++i)
+        concat[i] = string[i];
+    for (int j = l; j < l + suffixl; ++j)
+        concat[j] = suffix[j - l];
+    concat[l + suffixl] = '\0';
+    delete[] string;
+    string = concat;
+    l += suffixl;
+    return *this;
+	 };
+    String & operator+=(char suffix){
+	 char *concat = new char[l + 2];
+    for (int i = 0; i < l; ++i)
+        concat[i] = string[i];
+    concat[l] = suffix;
+    concat[l + 1] = '\0';
+    delete[] string;
+    string = concat;
+    l++;
+    return *this;
+	 };
+    void swap(String &other){
+	 char *tmp;
+    int tmpl;
+    tmp = string;
+    tmpl = l;
+    string = other.string;
+    l = other.l;
+    other.string = tmp;
+    other.l = tmpl;
+	 };
+    char & operator[](int pos){   
+	 return string[pos];
+	 };
+    const char operator[](int pos) const{
+	     return string[pos];
+	};
+    char & at(int pos){    
+	 if (pos >= l) 
+        throw std::out_of_range("");
+    return string[pos];
+	};
+    const char at(int pos) const{
+	  if (pos >= l) 
+        throw std::out_of_range("");
+    return string[pos];
+	 };
+    const char * data() const{
+	 return string;
+	 };
+    int size() const{
+	 return length;
+	 };
+bool operator==(const String &lhs, const String &rhs) {
+    if (lhs.l == rhs.l){
+        for (int i = 0; i < lhs.l; ++i)
+            if (lhs.string[i] != rhs.string[i])
+                return false;
+        return true;
+		  }
+    return false;
+	}
+bool operator<(const String &lhs, const String &rhs) {
+    int size;
+    size = lhs.l <= rhs.l ? lhs.l : rhs.l;
+    for (int i = 0; i < size; ++i)
+      if (lhs.string[i] < rhs.string[i])
+         return true;
+		else if (lhs.string[i] > rhs.string[i])
+         return false;
+    if (rhs.l > lhs.l)
+        return true;
+    return false;
+	}
 };
-String operator+(const String &lhs, const String &rhs);
-String operator+(const String &lhs, const char *rhs);
-String operator+(const char *lhs, const String &rhs);
-bool operator!=(const String &lhs, const String &rhs);
-bool operator<=(const String &lhs, const String &rhs);
-bool operator>(const String &lhs, const String &rhs);
-bool operator>=(const String &lhs, const String &rhs);
+String operator+(const String &lhs, const char *rhs) {
+    return String(lhs) += rhs;
+}
+
+String operator+(const char *lhs, const String &rhs) {
+    return String(rhs) += lhs;
+}
+
+bool operator!=(const String &lhs, const String &rhs) {
+    return !(lhs == rhs);
+}
+
+bool operator<=(const String &lhs, const String &rhs) {
+    return (lhs == rhs) || (lhs < rhs);
+}
+
+bool operator>(const String &lhs, const String &rhs) {
+    return !(lhs <= rhs);
+}
+
+bool operator>=(const String &lhs, const String &rhs) {
+    return !(lhs < rhs);
+}
 #endif
 
 
